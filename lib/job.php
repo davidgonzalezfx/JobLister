7976cd86524d75c505033e85f3509841352a8936
davidgonzalezfx <dvdramos16@gmail.com>
2019-11-23 19:25:42 -0500

    Filter by category

--- lib/job.php
+++ lib/job.php
@@ -21,6 +21,39 @@ class Job {
 
 		return $results;
 	}
+
+	public function getCategories(){
+		$this->db->query("SELECT * FROM categories");
+
+		$results = $this->db->resultSet();
+
+		return $results;
+	}
+
+	public function getByCategory($category) {
+		$this->db->query("SELECT jobs.*, categories.name AS cname
+								FROM jobs
+								INNER JOIN categories
+								ON jobs.category_id = categories.id
+								WHERE jobs.category_id = $category  
+								ORDER BY post_date DESC
+								");
+
+		$results = $this->db->resultSet();
+
+		return $results;
+	}
+
+	public function getCategory($category_id) {
+		$this->db->query("SELECT * FROM categories WHERE id = :category_id");
+		$this->db->bind(':category_id', $category_id);
+
+		$row = $this->db->single();
+		return $row;
+	}
+
 }
+
+
 
 ?>
